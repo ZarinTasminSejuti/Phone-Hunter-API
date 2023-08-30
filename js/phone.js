c77c14396f312d98835ac6a97996995b4c30b95f
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-08-30 18:38:16 +0300

    added cards and search section

---
+++ js/phone.js
@@ -0,0 +1,36 @@
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones => {
+
+    const phoneContainer = document.getElementById("phone-container");
+
+    phoneContainer.textContent = " "
+
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`
+        phoneCard.innerHTML = `<figure><img src="${phone.image}" alt="Shoes" /></figure>
+            <div class="card-body">
+              <h2 class="card-title">${phone.phone_name}</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Show Details</button>
+              </div>
+            </div>`;
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+
+//handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
